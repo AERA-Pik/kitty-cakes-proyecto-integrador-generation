0b10cc119b5cc19b50344e7ccead1274b0389879
Leticia-Somera <96146908+Leticia-Somera@users.noreply.github.com>
2022-02-22 01:35:10 +0000

    Deploying to gh-pages from @ AERA-Pik/kitty-cakes-proyecto-integrador-generation@8ad0cce19b4d037338de2cbbf90d0dbfa2c99259 🚀

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,3 +1,6 @@
+/* Aolicacion de loader 
+a cada una de las paginas con una clase en cada
+una de las paginas */
 window.onload=function(){
     /* alert("cargando...") */
      $('#onload').fadeOut(); 
